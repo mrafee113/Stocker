7cf6bcfe11edde14cd0cdac86c86fefa96d8a436
Mehdi <mrafee113@gmail.com>
2021-06-28 19:40:52 +0430

    remove bs

--- Stocker/settings.py
+++ Stocker/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rp80w%g6o0_26$!s=%2-j^tvf9)+)7$y-lk#--j*gqlxog2i8_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
